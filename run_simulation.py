cdc6af62bb24345d23ac558a8cf9b218ccee2df9
Pieter Senster <pietersenster@MacBook-Pro-van-Pieter.local>
2025-05-13 13:41:00 +0200

    work

--- run_simulation.py
+++ run_simulation.py
@@ -10,7 +10,7 @@ import subprocess
 python_executable = "./venv/bin/python3"
 
 # Command to run the simulation with the 36_Particles.zarr file
-command = [python_executable, "main.py", "circular", "--zarr", "pset/36_Particles.zarr"]
+command = [python_executable, "main.py", "lawnmower", "--zarr", "pset/36_Particles.zarr"]
 
 # Run the command
 print(f"Running command: {' '.join(command)}")
